26102461f823c6205043b2ca96aad03bb8c1906b
Chao Wu <chaowu@linux.alibaba.com>
2022-11-28 20:39:06 +0800

    log: set up log only when command is create
    Since now we have 2 commands, we should only set up log only when
    command is create so that when the command is connect the previous log
    won't get flushed.
    
    Signed-off-by: Chao Wu <chaowu@linux.alibaba.com>

--- src/main.rs
+++ src/main.rs
@@ -24,27 +24,25 @@ mod vmm_comm_trait;
 
 fn main() -> Result<()> {
     let args: DBSArgs = DBSArgs::parse();
-
-    let log_file = &args.log_file;
-    let log_level = Level::from_str(&args.log_level).unwrap();
-
-    let file = std::fs::OpenOptions::new()
-        .truncate(true)
-        .read(true)
-        .create(true)
-        .write(true)
-        .open(log_file)
-        .expect("Cannot write to the log file.");
-
-    let root = slog::Logger::root(
-        Mutex::new(slog_json::Json::default(file).filter_level(log_level)).map(slog::Fuse),
-        o!("version" => env!("CARGO_PKG_VERSION")),
-    );
-
-    let _guard = set_global_logger(root);
-
     match args.command {
         Some(Commands::Create) => {
+            let log_file = &args.log_file;
+            let log_level = Level::from_str(&args.log_level).unwrap();
+
+            let file = std::fs::OpenOptions::new()
+                .truncate(true)
+                .read(true)
+                .create(true)
+                .write(true)
+                .open(log_file)
+                .expect("Cannot write to the log file.");
+
+            let root = slog::Logger::root(
+                Mutex::new(slog_json::Json::default(file).filter_level(log_level)).map(slog::Fuse),
+                o!("version" => env!("CARGO_PKG_VERSION")),
+            );
+
+            let _guard = set_global_logger(root);
             run_with_cli(args)?;
         }
         Some(Commands::Connect) => {
